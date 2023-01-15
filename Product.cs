f5c2d1b5748e659227ed68fe7a6ee0acfc2f4b84
KozlovOleg97 <kozlovoleg2021@gmail.com>
2023-01-15 23:58:35 +0300

    Add Model

---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_OOP
+{
+    internal class Product
+    {
+        public string _name { get; private set; }
+        public int _quantity { get; private set; }
+        public decimal _price { get; private set; }
+
+        public Product(string name, int quantity, decimal price)
+        {
+            _name = name;
+            _quantity = quantity;
+            _price = price;
+        }
+
+        public Product()
+        {
+            
+        }
+    }
+}
